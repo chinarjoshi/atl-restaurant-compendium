3555e66d8a55f57b164195656f581108226711fb
Chinar Joshi <chinarhjoshi@gmail.com>
2022-08-01 15:42:58 -0400

    Add checks for missing data and remove unnecessary files

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ from pathlib import Path
 
 import pandas as pd
 from bs4 import BeautifulSoup
-from datetime import time, datetime
+from datetime import time
 
 from summarizer import summarizer
+from make_data import get_distance, get_site, gmaps
 
 data = Path("data")
 summarize = summarizer(3)  # 3 sentences of description
+
+if not (data / 'site.html').exists():
+    get_site(url='https://www.atlantamagazine.com/50bestrestaurants/', path=data/'site.html')
 
 # Step 1: Parse the soup and assemble information about each restaurant
 def build_restaurant(r: BeautifulSoup) -> dict[str, str]:
@@ -20,28 +24,30 @@ def build_restaurant(r: BeautifulSoup) -> dict[str, str]:
     website = r.select_one("em > a")
     website = website.text if website else 'None'
 
+    address = r.select_one("div.address").text
+    city = r.select_one("div.neighbor").text
+    car_dist = get_distance(gmaps, address, city, mode='driving')
+    bike_dist = get_distance(gmaps, address, city, mode='bicycling')
+
     return {
         "name": r.select_one("h2.restName").text,
         "description": summarize(r.select_one("div.restLeft > p").text),
         "price": price.text.count('$'),
-        "city": r.select_one("div.neighbor").text,
-        "address": r.select_one("div.address").text,
+        "city": city,
+        "address": address,
         "website": website,
+        "bike_dist": bike_dist,
+        "car_dist": car_dist,
+        "is_bikable": bike_dist < time(0, 20)
     }
 
 
 with open(data / "site.html") as f:
     soup = BeautifulSoup(f, "html.parser")
 
-restaurants = pd.DataFrame(
+df = pd.DataFrame(
     build_restaurant(r) for r in soup.select("div.restLRContainer")
 )
-distances = pd.read_csv(data / "dist.csv")
 
-df = restaurants.join(distances)
-
-def is_bikable(row) -> bool:
-    """Returns True if the restaurant is bikable."""
-    return datetime.strptime(row['bike_dist'], '%H:%M:%S').time() < time(0, 20)
-
-df['is_bikable'] = df.apply(is_bikable, axis=1)
+with open(data / 'output.csv', 'w') as f:
+    df.to_csv(f, index=False)
--- make_data.py
+++ make_data.py
@@ -1,10 +1,10 @@
-from googlemaps import Client
 from datetime import time
-from requests_html import HTMLSession
 from pathlib import Path
 
-maps = Client(key="AIzaSyDr7db_xqI9M5Cz3lx3EWsfgpiySAbVt3k")
-data = Path(__file__).parent / 'data'
+from googlemaps import Client
+from requests_html import HTMLSession
+
+gmaps = Client(key="AIzaSyDr7db_xqI9M5Cz3lx3EWsfgpiySAbVt3k")
 
 def get_distance(
     gmaps: Client,
@@ -14,7 +14,9 @@ def get_distance(
     mode: str = "driving",
 ) -> time:
     """Returns time distance between two points by a given mode of transport."""
-    dist = gmaps.distance_matrix(origin, f'{dest}, {city}', mode=mode)
+    dist = gmaps.distance_matrix(origin, f"{dest}, {city}", mode=mode)
+    if dist["rows"][0]["elements"][0]["status"] != "OK":
+        dist = gmaps.distance_matrix(origin, f"{dest}, Atlanta", mode=mode)
     dist = dist["rows"][0]["elements"][0]["duration"]["text"]
     tokens = dist.split()
     if "hour" in tokens:
@@ -22,13 +24,15 @@ def get_distance(
     else:
         return time(0, int(tokens[0]))
 
-def get_html(url: str) -> None:
-    """Returns html of a given url."""
-    if (data / 'site.html').exists():
+
+def get_site(url: str, path: Path) -> None:
+    """Writes fully rendered HTML to Path."""
+    data = Path(__file__).parent / "data"
+    if (data / "site.html").exists():
         return
 
     session = HTMLSession()
     site = session.get(url)
     site.html.render()
-    with open(data / 'site.html', 'w', encoding='utf-8') as f:
+    with open(path, "w", encoding="utf-8") as f:
         print(site.text, file=f)
